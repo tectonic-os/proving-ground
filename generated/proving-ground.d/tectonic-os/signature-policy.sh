a7540765b88afbda99229f7eb28c1700ab48d071
John Grant <john@johngrant.dev>
2026-08-21 00:15:41 +1000

    prove tect 0.3.0 in ci

--- generated/proving-ground.d/tectonic-os/signature-policy.sh
+++ generated/proving-ground.d/tectonic-os/signature-policy.sh
@@ -5,6 +5,4 @@ set -euxo pipefail
 MODDIR=/ctx/modules/.remote/tectonic-os/signature-policy
 export MODDIR
 
-cp -rT /ctx/modules/.remote/tectonic-os/signature-policy/files /
-
 install -D -m 0644 -- '/ctx/keys/etc/pki/containers/cosign.pub' '/etc/pki/containers/cosign.pub'
